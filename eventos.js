5c5ce725b9ffa767eb7695b2965ac45353e13d65
Ivanna Tuggener <ivatuggener@gmail.com>
2023-08-06 19:36:58 -0300

    solucionando error del div

--- eventos.js
+++ eventos.js
@@ -2,4 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("eldiv").addEventListener("click", function() {
       alert("Hola! Soy el div");
     });
+
+    document.querySelector("button").addEventListener("click", function(event) {
+      event.stopPropagation(); // Solución al problema
+    });
+
   });
